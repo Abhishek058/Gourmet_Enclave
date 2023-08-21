5e1bc726293889337271c2792c8a7119efcb7ec1
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-21 16:48:07 +0530

    ReEstablished all the main dependencies

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-5xl">Gourmet Enclave</h1>
+    </div>
+  );
+}
+
+export default App;
