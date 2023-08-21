86662610ba6ddb752c2500c269b98de09ffc7baf
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-21 14:04:16 +0530

    backend established

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+// backend/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/api/data', (req, res) => {
+  const data = { message: 'Hello from the backend!' };
+  res.json(data);
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server is running on port ${PORT}`);
+});
+
