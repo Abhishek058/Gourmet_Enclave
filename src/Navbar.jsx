feffe9f2988b5b11de7849ea6d8201ec87011304
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2023-08-25 03:31:23 +0530

    navBar completed || Responsiveness remaining

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch, FaBars } from "react-icons/fa";
 import { IoIosArrowDown } from "react-icons/io";
 
 function Navbar() {
+  const [showPages, setShowPages] = useState(false);
+  const [showBlog, setShowBlog] = useState(false);
+
   const items = [
     { id: 1, text: "HOME", link: "", arrow: false },
-    { id: 2, text: "PAGES", link: "", arrow: true },
+    { id: 2, text: "PAGES", arrow: true },
     { id: 3, text: "CONTACT US", link: "", arrow: false },
-    { id: 4, text: "BLOG", link: "", arrow: true },
+    { id: 4, text: "BLOG", arrow: true },
+  ];
+
+  const items2 = [
+    { text: "About Us", link: "" },
+    { text: "Services", link: "" },
+    { text: "Team", link: "" },
+    { text: "FAQ?", link: "" },
+  ];
+
+  const items3 = [
+    { text: "BLOG 1", link: "" },
+    { text: "BLOG 2", link: "" },
+    { text: "BLOG 3", link: "" }
   ];
 
   return (
@@ -18,15 +34,29 @@ function Navbar() {
           <li
             className="list-none font-roboto p-4 flex items-center"
             key={item.id}
+            onClick={() => {
+              if (item.text === "PAGES") {
+                if (showBlog) {
+                  setShowBlog(!showBlog);
+                }
+                setShowPages(!showPages);
+              }
+              if (item.text === "BLOG") {
+                if (showPages) {
+                  setShowPages(!showPages);
+                }
+                setShowBlog(!showBlog);
+              }
+            }}
           >
-            <a href={item.link} className="hover:cursor-pointer">
+            <a href={item.link} className="font-roboto hover:cursor-pointer">
               {item.text}
             </a>
             {item.arrow ? <IoIosArrowDown className="ml-1" /> : null}
           </li>
         ))}
       </div>
-      <div className="search bg-transparent flex items-center absolute right-10 border-b">
+      <div className="search bg-transparent hidden flex items-center absolute right-10 border-b">
         <input
           className="input-text text-gray-300 outline-none bg-transparent p-2"
           type="text"
@@ -36,6 +66,32 @@ function Navbar() {
       </div>
       <div className="hamBars absolute right-7">
         <FaBars className="text-white text-2xl" />
+      </div>
+      <div
+        className={`pages-div bg-black absolute top-24 ml-[-80px] w-40 h-48 ${
+          showPages ? "visible" : "invisible"
+        }`}
+      >
+        {items2.map((item) => (
+          <li className="list-none text-customYellow p-3 ml-5" key={item.text}>
+            <a href={item.link} className="hover:cursor-pointer">
+              {item.text}
+            </a>
+          </li>
+        ))}
+      </div>
+      <div
+        className={`pages-div bg-black absolute top-24 ml-[380px] w-40 h-48 ${
+          showBlog ? "visible" : "invisible"
+        }`}
+      >
+        {items3.map((item) => (
+          <li className="list-none text-customYellow p-3 ml-5" key={item.text}>
+            <a href={item.link} className="hover:cursor-pointer">
+              {item.text}
+            </a>
+          </li>
+        ))}
       </div>
     </div>
   );
