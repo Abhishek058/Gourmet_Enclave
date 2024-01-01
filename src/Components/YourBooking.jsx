963191607907fa20f987db7edba60dffb3f09df1
Abhishek058 <abhishek0058.be21@chitkara.edu.in>
2024-01-01 18:01:20 +0530

    no order error page bug fixed || loading animation added

--- src/Components/YourBooking.jsx
+++ src/Components/YourBooking.jsx
@@ -3,24 +3,42 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import ErrorPage from "./ErrorPage";
 import BookingModal from "./BookingModal";
+import loader from "../assets/loader.gif";
 
 function YourBooking() {
   const [order, setOrder] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
+    setLoading(true);
+
     axios
       .get(`https://gourmet-enclave.onrender.com/orders/${id}`)
       .then((response) => {
         setOrder(response.data.data);
+        setLoading(false);
       })
       .catch((error) => {
         console.log(error);
+        setLoading(false);
         alert("Order not found");
       });
   }, [id]);
 
-  return <>{order ? <BookingModal order={order} /> : <ErrorPage />}</>;
+  return (
+    <>
+      {loading ? (
+        <div className="loader-div">
+          <img src={loader} alt="Loading..." />
+        </div>
+      ) : order.length !== 0 ? (
+        <BookingModal order={order} />
+      ) : (
+        <ErrorPage />
+      )}
+    </>
+  );
 }
 
 export default YourBooking;
